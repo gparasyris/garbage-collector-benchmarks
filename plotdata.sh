db7a509b74ba7ea6062f6d3570d18d274d9e4cea
George Parasyris <gparasir@gmail.com>
2018-05-18 19:07:41 +0300

    pending dir params

--- plotdata.sh
+++ plotdata.sh
@@ -1,6 +1,6 @@
 # #!/bin/bash
-python3 allocationFailures.py
-./count-pause.sh
-python3 durationplot.py
-python3 pause-duration.py
-python3 scoreplot.py
+python3 generate-plots/allocationFailures.py
+./generate-plots/count-pause.sh
+python3 generate-plots/durationplot.py
+python3 generate-plots/pause-duration.py
+python3 generate-plots/scoreplot.py
